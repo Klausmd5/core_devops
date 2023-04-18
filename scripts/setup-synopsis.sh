86c305cfcaf0d32df00ed7bb667f839326be7163
Fabian Gurtner <fabian.paul.gurtner@gmail.com>
2023-04-18 22:56:56 +0200

    Also download .env file

--- scripts/setup-synopsis.sh
+++ scripts/setup-synopsis.sh
@@ -1,5 +1,6 @@
 curl -O https://raw.githubusercontent.com/htl-grieskirchen-core/devops/develop/docker-compose.yml
 curl -O https://raw.githubusercontent.com/htl-grieskirchen-core/devops/develop/mainframe-config.json
+curl -O https://raw.githubusercontent.com/htl-grieskirchen-core/devops/develop/.env
 
 mkdir public-keys
 mkdir private-keys
